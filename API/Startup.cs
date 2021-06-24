8eee393f7689a8d6f102303b8d559899fe5336c6
Santos <max.go.95@gmail.com>
2021-06-23 20:23:56 -0500

    Basic React component that fetches from db implemented

--- API/Startup.cs
+++ API/Startup.cs
@@ -56,7 +56,7 @@ namespace API
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
             }
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
