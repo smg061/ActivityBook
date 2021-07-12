41251ad55b4585b711ddc800e29b4a1992288cc6
smg061 <max.go.95@gmail.com>
2021-07-11 22:22:27 -0500

    axios request changes

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -26,7 +26,7 @@ const requests = {
   get: <T>(url: string) => axios.get<T>(url).then(responseBody),
   post: <T>(url: string, body: {}) => axios.post<T>(url, body).then(responseBody),
   put: <T>(url: string, body: {}) => axios.put<T>(url, body).then(responseBody),
-  del: <T>(url: string) => axios.delete<T>(url).then(responseBody),
+  del: <T>(url: string) => axios.delete<T>(url, {headers:{"Content-Type": "application/json"}}).then(responseBody),
 };
 
 const Activities = {
@@ -34,7 +34,8 @@ const Activities = {
   details: (id: string) => requests.get<Activity>(`/activities/${id}`),
   create: (activity: Activity) => requests.post<void>("/activities", activity),
   update: (activity: Activity) => requests.put<void>(`/activities/${activity.id}`, activity),
-  delete: (id: string) => requests.del<void>(`/activities/${id}`),
+  delete: (id: string) => requests.del<void>(`/activities/${id}`)
+  
 };
 
 const agent = {
